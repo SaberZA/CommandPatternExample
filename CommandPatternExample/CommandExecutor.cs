ce46f00b0d4a358268842fea5c438a0b635d1a64
Steven van der Merwe <stevenvdmerwe@gmail.com>
2014-11-06 05:54:28 +0200

    Initial commit

---
+++ CommandPatternExample/CommandExecutor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CommandPatternExample
+{
+    class CommandExecutor
+    {
+        public void ExecuteCommand(string[] args)
+        {
+            switch (args[0])
+            {
+                case "UpdateQuantity":
+                    UpdateQuantity(int.Parse(args[1]));
+                    break;
+                case "CreateOrder":
+                    CreateOrder();
+                    break;
+                case "ShipOrder":
+                    ShipOrder();
+                    break;
+                default:
+                    Console.WriteLine("Unrecognised command");
+                    break;
+            }
+        }
+
+        private void ShipOrder()
+        {
+            
+        }
+
+        private void CreateOrder()
+        {
+            
+        }
+
+        private void UpdateQuantity(int newQuantity)
+        {
+            //simulate updating db
+            int oldQuantity = 5;
+            Console.WriteLine("Database: Updated");
+
+            //simulate logging
+            Console.WriteLine("LOG: Updated order quantity from {0} to {1}",oldQuantity,newQuantity);
+        }
+    }
+}
