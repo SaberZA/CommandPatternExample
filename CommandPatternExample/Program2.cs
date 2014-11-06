f78ec73bbca936cf59a7036fb664ca4aec661c89
Steven van der Merwe <stevenvdmerwe@gmail.com>
2014-11-06 06:47:04 +0200

    Unsaved files

--- CommandPatternExample/Program2.cs
+++ CommandPatternExample/Program2.cs
@@ -78,14 +78,4 @@ namespace CommandPatternExample
                 .FirstOrDefault(cmd => cmd.CommandName == requestedCommandName);
         }
     }
-
-    public class NotFoundCommand : ICommand
-    {
-        public string Name { get; set; }
-
-        public void Execute()
-        {
-            Console.WriteLine("Couldn't find command: "+Name);
-        }
-    }
 }
